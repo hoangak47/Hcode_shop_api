11224915ce6f0e1302a784670d7b6eb0ff1960a8
hoangak47 <hoang41911@gmail.com>
2023-06-09 23:21:55 +0700

    update

--- Models/MongooseDB.model.js
+++ Models/MongooseDB.model.js
@@ -1,24 +1,24 @@
 module.exports = {
   findAllDB: async (table) => {
-    const db = await require("../initDB")();
+    const db = await require("./initDB")();
     return await db.db().collection(table).find({}).toArray();
   },
   findDB: async (query, table, sort = null) => {
-    const db = await require("../initDB")();
+    const db = await require("./initDB")();
     return await db.db().collection(table).find(query).sort(sort).toArray();
   },
   updateDB: async (query, data, table) => {
-    const db = await require("../initDB")();
+    const db = await require("./initDB")();
     await db.db().collection(table).updateOne(query, {
       $set: data,
     });
   },
   insertDB: async (data, table) => {
-    const db = await require("../initDB")();
+    const db = await require("./initDB")();
     await db.db().collection(table).insertOne(data);
   },
   deleteDB: async (query, table) => {
-    const db = await require("../initDB")();
+    const db = await require("./initDB")();
     console.log(query);
     await db.db().collection(table).deleteOne(query);
   },
