c828f5c9f4ae093938910eb5131518a83ee4af61
hoangak47 <hoang41911@gmail.com>
2023-06-09 21:41:39 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
 const parser = require("body-parser");
 app.use(express.json());
-app.use(cors());
 app.use(parser.urlencoded({ extended: true }));
 
 const dotenv = require("dotenv");
