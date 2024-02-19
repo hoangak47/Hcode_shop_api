1511c06a0aec9182547c3408adee7f823d269d95
hoangak47 <nguyenhoang41911@email.com>
2024-02-19 22:52:31 +0700

    update

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 var cors = require("cors");
 
 const app = express();
+
 app.use(
   cors({
     origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 
